f8cace37e4ec65f084a8ade20eabe03bae9a924f
Samuel Toth <sam@toth.co.uk>
2025-11-13 19:50:00 +0000

    Fixing CI - move agda_datadir

--- default.nix
+++ default.nix
@@ -38,7 +38,7 @@ in
       mkdir -p ./output
       mkdir -p ./output/html
       echo "made /output/html dir"
-      LC_ALL=C.UTF-8 agda-forester --forest -otrees/stt/autogen --fhtml-dir=output/html src/Everything.agda
+      LC_ALL=C.UTF-8 Agda_datadir=./_build agda-forester --forest -otrees/stt/autogen --fhtml-dir=output/html src/Everything.agda
       echo "Generated trees"
       forester build
       cp ./output/agda-synthetic-categories/Agda.css ./output/html/Agda.css

6c7cce95794ca2bcc520b650c7c700587eae1992
Samuel Toth <sam@toth.co.uk>
2025-11-13 22:33:33 +0000

    Setup root address

--- default.nix
+++ default.nix
@@ -41,12 +41,13 @@ in
       LC_ALL=C.UTF-8 Agda_datadir=./_build agda-forester --forest -otrees/stt/autogen --fhtml-dir=output/html src/Everything.agda
       echo "Generated trees"
       forester build
-      cp ./output/Agda.css ./output/html/Agda.css
+      cp ./output/agda-synthetic-categories/Agda.css ./output/html/Agda.css
     '';
 
     installPhase = ''
       echo $out
       mkdir -p $out
-      cp -Lrvf output/* "$out"/
+      cp -Lrvf output/agda-synthetic-categories/* "$out"/
+      cp -Lrvf output/html "$out"/
     '';
   }

c10b3c993dd86f057b572f0219eaf0f08354c257
Fredrik Bakke <fredrbak@gmail.com>
2026-05-21 23:57:08 +0200

    run pre-commit again

--- shell.nix
+++ shell.nix
@@ -1,24 +1,26 @@
 {
-system ? builtins.currentSystem
+  system ? builtins.currentSystem,
 }:
 let
   pkgs = import <nixpkgs> { inherit system; };
   af = (import ./nix/agda-forester { inherit system; });
   drv = (import ./default.nix { inherit system; });
   forester-server = builtins.getFlake "github:kentookura/forest-server?rev=d33690f372ae0ba87df48b59b5deb7148752839b";
-in pkgs.mkShell {
-    name = "agda-forester-shell";
+in
+pkgs.mkShell {
+  name = "agda-forester-shell";
 
-    buildInputs = [
-      (forester-server.packages.${system}.default)
-      (pkgs.python3.withPackages (py-pkgs: []))
-      (pkgs.codespell)
-      (pkgs.ripgrep)
-    ] ++ drv.buildInputs;
+  buildInputs = [
+    (forester-server.packages.${system}.default)
+    (pkgs.python3.withPackages (py-pkgs: [ ]))
+    (pkgs.codespell)
+    (pkgs.ripgrep)
+  ]
+  ++ drv.buildInputs;
 
-    shellHook = ''
+  shellHook = ''
     echo "Welcome to the dev shell!"
     echo "Agda version: $(agda --version)"
     echo "Forester version: $(forester --version || echo not found)"
-    '';
+  '';
 }
